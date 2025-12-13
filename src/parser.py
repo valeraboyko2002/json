23c67816d129cc7cc1f944549ae40ea6e8557919
valeraboyko2002 <228337dima013@gmail.com>
2025-12-13 23:10:49 +0300

    parser почти закончен.

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Union
+from typing import Any, Dict, List, Union, Optional
 from .exceptions import JSONSyntaxError, JSONDepthError
 from .lexer import JSONLexer
 
@@ -7,11 +7,11 @@ class JSONParser:
 
     MAX_DEPTH = 1000
 
-    def __init__(self,max_depth: int = None):
+    def __init__(self, max_depth: Optional[int] = None):
         self.max_depth = max_depth or self.MAX_DEPTH
         self.current_depth = 0
 
-    def parse(self, json_string: str) -> Union[Dict,list]:
+    def parse(self, json_string: str) -> Union[Dict, List]:
         """Основной метод парсинга JSON-строки."""
         lexer = JSONLexer(json_string)
         self.current_depth = 0
@@ -29,7 +29,7 @@ class JSONParser:
         
         return result
     
-    def _parse_value(self,lexer: JSONLexer, token: tuple):
+    def _parse_value(self, lexer: JSONLexer, token: tuple) -> Any:
         """Парсинг значений JSON"""
         token_type, token_value = token
 
@@ -41,7 +41,93 @@ class JSONParser:
             return token_value
         else:
             raise JSONSyntaxError(f"Ожидалось значение, получено: {token_type}")
-        
     
-    def _parse_object(self,lexer: JSONLexer) -> Dict:
+    def _parse_object(self, lexer: JSONLexer) -> Dict:
+        """Парсинг JSON-объекта"""
+        self._check_depth()
+        self.current_depth += 1 
+
+        result = {}
+        first = True
+
+        while True:
+            token = lexer.get_next_token()
+
+            if token is None:
+                raise JSONSyntaxError("Незакрытый объект")
+            
+            if token[0] == 'RBRACE':
+                break
+            
+            if not first:
+                if token[0] != 'COMMA':
+                    raise JSONSyntaxError('Ожидалась запятая между элементами')
+                token = lexer.get_next_token()
+            
+            if token[0] != 'STRING':
+                raise JSONSyntaxError(f"Ключ объекта должен быть строкой, получено: {token[0]}")
+            
+            key = token[1]
+
+            colon_token = lexer.get_next_token()
+            if colon_token is None or colon_token[0] != 'COLON':
+                raise JSONSyntaxError('Ожидалось двоеточие после ключа')
+            
+            value_token = lexer.get_next_token()
+            if value_token is None:
+                raise JSONSyntaxError('Ожидалось значение')
+            
+            result[key] = self._parse_value(lexer, value_token)
+            first = False
+        
+        self.current_depth -= 1
+        return result
+    
+    def _parse_array(self, lexer: JSONLexer) -> List:
+        """Парсинг JSON-массива (ДОБАВЬТЕ ЭТОТ МЕТОД!)"""
+        self._check_depth()
+        self.current_depth += 1
+
+        result = []
+        first = True
+
+        while True:
+            token = lexer.get_next_token()
+
+            if token is None:
+                raise JSONSyntaxError("Незакрытый массив")
+            
+            if token[0] == 'RBRACKET':
+                break
+            
+            if not first:
+                if token[0] != 'COMMA':
+                    raise JSONSyntaxError('Ожидалась запятая между элементами массива')
+                token = lexer.get_next_token()
+            
+            result.append(self._parse_value(lexer, token))
+            first = False
+        
+        self.current_depth -= 1
+        return result
+    
+    def _check_depth(self):
+        """Проверка глубины вложенности"""
+        if self.current_depth >= self.max_depth:
+            raise JSONDepthError(
+                f'Превышена максимальная глубина вложенности: {self.max_depth}'
+            )
+    
+    @staticmethod
+    def parse_file(filepath: str, **kwargs) -> Union[Dict, List]:
+        """Парсинг JSON из файла"""
+        with open(filepath, 'r', encoding='utf-8') as f:
+            content = f.read()
+        parser = JSONParser(**kwargs)
+        return parser.parse(content)
+
+    @staticmethod
+    def parse_stream(stream, chunk_size: int = 8192, **kwargs):
+        """Потоковый парсинг больших JSON файлов"""
+        # TODO: реализовать потоковый парсинг (хахахах)
         pass
